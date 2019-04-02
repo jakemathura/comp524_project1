2a03b81506d1438ce1f92a80b02609fd893aab6e
Madhu <gvadlamani1@gmail.com>
2019-04-01 20:14:21 -0400

    all my stuff

---
+++ project1/SecurityDelaysMin/SecurityDelaysReducer.java
@@ -0,0 +1,37 @@
+// reducer function for application to count the number of
+// times each unique IP address 4-tuple appears in an
+// adudump file.
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.util.*;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class SecurityDelaysReducer extends Reducer<Text, LongWritable, Text, Text> {
+
+        public void reduce(Text key, Iterable<LongWritable> values, Context context)
+                        throws IOException, InterruptedException {
+
+                long total = 0;
+                long avg = 0;
+                long count = 0;
+                long stdev = 0;
+                List<LongWritable> list = new ArrayList<LongWritable>();
+
+                // iterate through all the values (count == 1) with a common key
+                for (LongWritable value : values) {
+                        list.add(value);
+                        total += value.get();
+                        count = count + 1;
+                }
+                avg = total/count;
+        for (LongWritable value : list) {
+            stdev = (value.get() - avg) * (value.get() - avg) + stdev;
+        }
+        stdev = stdev/count;
+        stdev = (long) Math.sqrt(stdev);
+
+                 context.write(key, new Text("Total:" + Long.toString(total) + " Avg:"+Long.toString(avg) + " Stdev:"+ Long.toString(stdev)));
+        }
+}
---
+++ project1/TotalDelaysMin/TotalDelaysMapper.java
@@ -0,0 +1,35 @@
+// map function for application to count the number of
+// times each unique IP address 4-tuple appears in an
+// adudump file.
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.util.*;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class TotalDelaysMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+
+	@Override
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		String line = value.toString();
+		String[] tokens = line.split(",");
+
+		String airlineCarrier = tokens[2];
+		String year = tokens[0];
+		long totalFlights = 0;
+
+		try {
+			totalFlights = Long.parseLong(tokens[15]);
+
+		} catch (Exception e) {
+
+		}
+
+		String yearPlusCarrier = year + " " + airlineCarrier;
+
+		context.write(new Text(yearPlusCarrier), new LongWritable(totalFlights));
+
+	}
+}
